30c93e31c15eba7c2e3c3ed904651aaa97bd045a
Filippo <filippocucina5@gmail.com>
2024-01-04 14:01:38 -0400

    This js code will change

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,12 @@
 const myHeading = document.querySelector("h1");
 myHeading.textContent = "Computer Science";
+
+/* document.querySelector("html").addEventListener("click", function() {
+	alert("Ouch! Stop poking me!!");
+});  */
+
+document.querySelector("html").addEventListener("click", () => {
+	alert("Ouch! Stop poking me!!");
+});  
+
+
