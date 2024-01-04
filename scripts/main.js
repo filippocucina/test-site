3c7de237c96cc81aa7d4864a2623a204310fc781
Filippo <filippocucina5@gmail.com>
2024-01-04 14:24:53 -0400

    Code for CChanging images

--- scripts/main.js
+++ scripts/main.js
@@ -3,10 +3,10 @@ const myImage = document.querySelector("img");
 myImage.onclick = () => {
   const mySrc = myImage.getAttribute("src");
 
-  if (mySrc === "images/firefox-icon.png") {
-    myImage.setAttribute("src", "images/firefox2.png");
+  if (mySrc === "images/lambda-png-9.png") {
+    myImage.setAttribute("src", "images/turing-machine.png");
   } else {
-    myImage.setAttribute("src", "images/firefox-icon.png");
+    myImage.setAttribute("src", "images/lambda-png-9.png");
   }
 };
 
