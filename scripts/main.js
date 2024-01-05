55d049ef0cbea7523236f1629f4ec695ad0f3352
Filippo <filippocucina5@gmail.com>
2024-01-05 16:36:53 -0400

    Function for the Button and the logic of the heading without using f{}

--- scripts/main.js
+++ scripts/main.js
@@ -10,3 +10,31 @@ myImage.onclick = () => {
   }
 };
 
+//Personalized welcome message code
+
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+
+function setUserName() {
+	let myName = prompt("Please enter your name.");
+	
+	if (!myName) {
+		setUserName()
+	} else {
+		localStorage.setItem("name", myName);
+		myHeading.textContent = 'Mozilla is cool, ' + myName;
+	}
+}
+
+
+if (!localStorage.getItem("name")) {
+	setUserName();
+} else {
+	let storedName = localStorage.getItem("name");
+	myHeading.textContent = 'Computer Science is cool!, ' + storedName;
+}
+
+
+myButton.onclick = () => {
+	setUserName();
+};
